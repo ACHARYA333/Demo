cd056140be4fe560d930e71d6787fbf42ec17dc7
ACHARYA333 <149923538+ACHARYA333@users.noreply.github.com>
2024-01-08 12:01:20 +0530

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -1,13 +1,13 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header("Types of Cats")
+st.set_page_config(page_title='Joker')
+st.header("Catagories")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Persian Cat")
-  st.image("./Persian.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.subheader("Joker")
+  st.image("./joker-the-dark-knight-heath-ledger-movies-wallpaper.jpg", caption="Persian Cat", width=300,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
-  st.subheader("Ragdoll Cat")
-  st.image("./ragdoll.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.subheader("Joker and Batman")
+  st.image("./HD-wallpaper-batman-joker-film-knight.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
   st.write("Ragdoll cats are proud")
