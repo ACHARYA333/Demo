fe585f0a9e02b675c66f04db7808c7cb96151c51
ACHARYA333 <149923538+ACHARYA333@users.noreply.github.com>
2024-01-08 12:12:47 +0530

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -5,9 +5,9 @@ st.header("Catagories")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("Joker")
-  st.image("./joker-the-dark-knight-heath-ledger-movies-wallpaper.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./joker-the-dark-knight-heath-ledger-movies-wallpaper.jpg", caption="Joker", width=300,use_column_width=True)
   st.write("Legendary Joker")
 with col2:
   st.subheader("Joker and Batman")
-  st.image("./HD-wallpaper-batman-joker-film-knight.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("./HD-wallpaper-batman-joker-film-knight.jpg", caption="Batman", width=300,use_column_width=True)
   st.write("Legendary Batman")
